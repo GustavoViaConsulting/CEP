8ac2df72c468eb0aae5fc34a7fd85b49d1a86f92
AlvesM97 <gutomorais2002@gmail.com>
2023-10-03 09:58:27 -0300

    Update consultarCep.controller.js

--- assets/controllers/consultarCep.controller.js
+++ assets/controllers/consultarCep.controller.js
@@ -2,19 +2,35 @@ consultarCep.controller("consultarCep", [
   "$scope",
   "viaCep",
   function ($scope, viaCep) {
-    $scope.consultarCep = function () {
-      viaCep
-        .consultarCep($scope.cep)
-        .then(function (data) {
-          $scope.logradouro = data.logradouro;
-          $scope.complemento = data.complemento;
-          $scope.bairro = data.bairro;
-          $scope.localidade = data.localidade;
-          $scope.uf = data.uf;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    var prosseguir = false;
+    var url;
+    cliente.metadata().then(function (parameters) {
+      url = parameters.settings.url;
+      prosseguir = true;
+    });
+
+    $scope.consultarCep = function (cep) {
+      if (prosseguir == true) {
+        viaCep
+          .consultarCep(url, cep)
+          .then(function (data) {
+            $scope.logradouro = data.logradouro;
+            $scope.complemento = data.complemento;
+            $scope.bairro = data.bairro;
+            $scope.localidade = data.localidade;
+            $scope.uf = data.uf;
+          })
+          .catch(function (error) {
+            console.log(error);
+            mensagemDeErro("Erro ao consultar cep");
+          });
+      } else {
+        mensagemDeErro("Erro ao consultar cep");
+      }
     };
+
+    function mensagemDeErro(mensagem) {
+      cliente.invoke("notify", "<b>" + mensagem + "</b>", "error", 3000);
+    }
   },
 ]);
